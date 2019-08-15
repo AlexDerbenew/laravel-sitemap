99bcd93983b9908d004f772c3d06bfb3c7953dff
AlexDerbenew <alex.derbenew@yandex.ru>
2019-08-15 18:02:06 +0300

    Added images to xml's namespaces

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,6 +1,9 @@
 <?= '<'.'?'.'xml version="1.0" encoding="UTF-8"?>'."\n"; ?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
-@foreach($tags as $tag)
-    @include('laravel-sitemap::' . $tag->getType())
-@endforeach
+<urlset
+    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+    xmlns:xhtml="http://www.w3.org/1999/xhtml"
+    xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+    @foreach($tags as $tag)
+        @include('laravel-sitemap::' . $tag->getType())
+    @endforeach
 </urlset>
